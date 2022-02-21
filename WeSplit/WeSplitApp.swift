4f9ca246edee9db9dc0f0f46e4bf7695ee5ef2b0
YossiBenZaken <yossibz@gmail.com>
2022-02-21 16:45:45 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yosef Ben Zaken on 21/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
